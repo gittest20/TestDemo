b9326dce05d2058e3fcc7ee4e947251d870305f8
DESKTOP-UGQULIG\polarium <sung053@polarium.co.kr>
2020-06-23 14:37:32 +0900

    건드리지 말것 메시지.

--- src/demo/test/main/TestMain.java
+++ src/demo/test/main/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1");
+		System.out.println("test2"); //브랜치 테스트용! 건드리지 말것!
 
 	}
 
